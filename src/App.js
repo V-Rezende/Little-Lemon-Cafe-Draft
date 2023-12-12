ffab62849ded3f8ef80ef18030fbd85c66962d84
Vinni Rezende <vinnirezende1980@gmail.com>
2023-12-12 10:52:36 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
@@ -9,8 +10,67 @@ import Footer from './Components/Footer';
 import Reservation from './Components/Reservation';
 import Order from './Components/Order';
 import Login from './Components/Login';
+import { useState, useReducer, useEffect } from 'react';
+import { fetchAPI, submitAPI } from './Components/apiUtils'; // Correct the path as needed
+
 
 function App() {
+
+  //Move availableTimes state to App component
+  {/* const [availableTimes, setAvailableTimes] = useState([
+    "6:00",
+    "7:00",
+    "8:00",
+    "9:00",
+    "10:00",
+    "11:00",
+    "12:00"
+]); */}
+
+  const seededRandom = function (seed) {
+    var m = 2**35 - 31;
+    var a = 185852;
+    var s = seed % m;
+    return function () {
+        return (s = s * a % m) / m;
+    };
+  }
+
+  const fetchAPI = function(date) {
+    const result = [];
+    const random = seededRandom(date.getDate());
+
+    for(let i = 6; i <= 12; i++) {
+      if(random() < 0.5) {
+        result.push(i + ":00");
+      }
+      if(random() > 0.5) {
+        result.push(i + ":30");
+      }
+    }
+    return result;
+  }
+
+  const submitAPI = function(formData) {
+    return true;
+  }
+
+  const initialState = {availableTimes: fetchAPI(new Date())}
+
+  const [state, dispatch] = useReducer(updateTimes, initialState);
+
+  //Update the updateTimes function that you previously created to use the fetchData API function. Remember, you dispatched the selected date to the updateTimes function. This should be passed to the fetchData function as a parameter.
+
+  function updateTimes(state, date) {
+    return {availableTimes: fetchAPI(new Date(date))}
+  }
+
+  function submitForm (formData) {
+    if(submitAPI(formData)) {
+      alert("Your table is Reserved. We are looking foward to see you!")
+    }
+  }
+
   return (
     <div className="App">
         <>
@@ -21,13 +81,22 @@ function App() {
                 <Route path="/" element={<Main />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/menu" element={<Menu />} />
-                <Route path="/reservation" element={<Reservation />} />
+                <Route
+                  path="/reservation"
+                  element={
+                  <Reservation
+                    availableTimes={state}
+                    dispatch={dispatch} //Passing dispatch function as a prop
+                    submitForm={submitForm} //Pass the callback function as a prop
+                    /> }
+                  />
                 <Route path="/order-online" element={<Order />} />
                 <Route path="/login" element={<Login />} />
               </Routes>
             </div>
           <Footer/>
         </>
+
     </div>
   )
 }
--- src/Components/Reservation.js
+++ src/Components/Reservation.js
@@ -1,36 +1,114 @@
 import styles from "./Reservation.css";
+import { useState } from "react";
+//import { submitAPI } from './apiUtils'; // Correct the path as needed
 
-function Reservations () {
+
+function Reservations (props) {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState ('');
+    const [number, setNumber] = useState ('');
+    const [date, setDate] = useState ('');
+    const [time, setTime] = useState ('6:00');
+    const [guests, setGuests] = useState ('');
+
+    const clearInput = () => {
+        setName('');
+        setEmail('');
+        setNumber('');
+        setDate('');
+        setTime('6:00');
+        setGuests('')
+    }
+
+    //Function to handle form submission in Reservation component
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        props.submitForm(e);
+        clearInput();
+        if (name.length < 3 || number.length < 9) {
+            alert("Name must be at least 3 characters. Phone must be at least 10 characters.");
+            return;
+        }
+    }
+    const handleChange = (e) => {
+        setDate(e);
+        props.dispatch(e);
+    };
+
     return (
         <div className="reservation">
-            <div className="form">
-                <h1>Reserve a Table for your spacial ocasion:</h1>
+            <form className="form" onSubmit={handleSubmit}>
+                <h1 className="heading">Reserve a Table:</h1>
 
-                    <label for="name">Name:</label>
-                    <input type="text" id="name" name="name" required/>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Enter your name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required/>
 
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" required/>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Enter a valid email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required/>
 
-                    <label for="phone">Phone:</label>
-                    <input type="tel" id="phone" name="phone" required/>
+                    <label htmlFor="phone">Phone:</label>
+                    <input
+                        type="tel"
+                        id="phone"
+                        name="phone"
+                        placeholder="Enter a phone number"
+                        value={number}
+                        onChange={(e) => setNumber(e.target.value)}
+                        required/>
 
-                    <label for="date">Preferred Date:</label>
-                    <input type="date" id="date" name="date" required/>
+                    <label htmlFor="book-date">Preferred Date:</label>
+                    <input
+                        type="date"
+                        id="book-date"
+                        value={date}
+                        onChange={(e) => handleChange(e.target.value)}
+                        name="date"
+                        required/>
 
-                    <label for="time">Preferred Time:</label>
-                    <input type="time" id="time" name="time" required/>
+                    <label htmlFor="book-time">Preferred Time:</label>
+                    <select
+                        type="time"
+                        id="book-time"
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}>
+                            <option value="" >Select Time</option>
+                        {/*Populate option from the availableTimes prop */}
+                        {props.availableTimes.availableTimes.map(availableTimes => {return <option key={availableTimes}>{availableTimes}</option>})}
 
-                    <label for="guests">Number of Guests:</label>
-                    <select id="guests" name="guests" required>
+                    </select>
+
+                    <label htmlFor="book-guests">Number of Guests:</label>
+                    <select
+                        id="book-guests"
+                        value={guests}
+                        onChange={(e) => setGuests(e.target.value)}
+                        name="guests"
+                        required>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
                         <option value="4">4</option>
                     </select>
 
-                    <button type="submit">Book Table</button>
-            </div>
+                    <button type="submit" >
+                            Book a Table
+                    </button>
+            </form>
         </div>
     )
 }
